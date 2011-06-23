cdc22afe480b9cd7b3db2d0a586dfcb5f04fdb0e
Greg Danner <Greg.Danner@Examiners.Alabama.gov>
2011-06-23 10:27:58 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b1e5bbaa07f5f6168081e7a43d29d20b705ce12a15ee0d60148959cc78c0ceb6b469a130eb7f144f16ee60cfec6451d75d9347d205379f545a0c07dca014d0a3'
